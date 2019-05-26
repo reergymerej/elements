fa6a15a72e0f83c27af901ca432f816a91a9722a
Jeremy Greer <jex.grizzle@gmail.com>
2019-05-26 10:04:32 -0700

    Handle unknown elements

--- lib/elements_web/controllers/page_controller.ex
+++ lib/elements_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule ElementsWeb.PageController do
   use ElementsWeb, :controller
 
+  @unknown "???"
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
@@ -8,12 +10,45 @@ defmodule ElementsWeb.PageController do
   def view(conn, %{"symbol" => symbol}) do
     # %{"foo" => "bar"} is a map
 
-    render(conn, "view-element.html",
-      symbol: symbol,
-      atomic_number: 99
+    render(
+      conn,
+	get_template(symbol),
+        get_element(symbol)
     )
+
     # assigns - a dictionary with the assigns to be used in the view. Those
     # assigns are merged and have higher precedence than the connection assigns
     # (conn.assigns)
+  end
+
+  defp get_template(symbol) do
+    case get_element(symbol) do
+      %{symbol: @unknown} -> "view-unknown-element.html"
+      _ -> "view-element.html"
+    end
+  end
+
+  defp get_element(symbol) do
+    case symbol do
+      "h" -> %{
+        symbol: "h",
+        name: "Hydrogen",
+        atomic_number: 1,
+      }
+
+      "he" -> %{
+        symbol: "he",
+        name: "Helium",
+        atomic_number: 2,
+      }
+
+      "li" -> %{
+          symbol: "li",
+          name: "Lithium",
+          atomic_number: 3,
+      }
+
+      _ -> %{ symbol: @unknown }
+    end
   end
 end
