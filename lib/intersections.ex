3d7a59a8b0e1aa9d1ddfde565c598023ee513b68
Jeremy Greer <jex.grizzle@gmail.com>
2019-06-01 16:29:58 -0700

    Fix range intersection check

--- lib/intersections.ex
+++ lib/intersections.ex
@@ -1,12 +1,6 @@
 defmodule ElementsWeb.Intersections do
-  def is_within_range(value, {left, right}) do
-    left <= value
-    and value <= right
-  end
-
-  def ranges_intersect({left, right}, rng) do
-    is_within_range(left, rng)
-    || is_within_range(right, rng)
+  def ranges_intersect({left, right}, {min, max}) do
+    not (right < min or left > max)
   end
 
   def boxes_intersect(a, b) do
--- test/elements_web/intersections_test.exs
+++ test/elements_web/intersections_test.exs
@@ -2,32 +2,23 @@ defmodule ElementsWeb.IntersectionsTest do
   use ExUnit.Case
   import ElementsWeb.Intersections
 
-  test "is_within_range" do
-    assert is_within_range(1, {0, 3}) == true
-    assert is_within_range(0, {1, 3}) == false
-    assert is_within_range(4, {0, 3}) == false
-    assert is_within_range(4, {0, 4}) == true
-  end
-
   test "ranges_intersect" do
     assert ranges_intersect({0, 3}, {1, 4}) == true
     assert ranges_intersect({0, 3}, {4, 5}) == false
+    assert ranges_intersect(
+      { 0, 9 },
+      { 3, 8 }
+    ) == true
+    assert ranges_intersect(
+      { 3, 8 },
+      { 0, 9 }
+    ) == true
   end
 
   test "boxes_intersect" do
     assert boxes_intersect(
-      %{left: 0, right: 10, top: 0, bottom: 10},
-      %{left: 0, right: 10, top: 50, bottom: 60}
-    ) == false
-
-    assert boxes_intersect(
-      %{left: 0, right: 10, top: 0, bottom: 10},
-      %{left: 0, right: 10, top: 5, bottom: 60}
+      %{ top: 0, bottom: 15, left: 0, right: 9 },
+      %{ top: 2, bottom: 16, left: 3, right: 8 }
     ) == true
-
-    assert boxes_intersect(
-      %{left: 100, right: 200, top: 0, bottom: 10},
-      %{left: 0, right: 10, top: 5, bottom: 60}
-    ) == false
   end
 end
