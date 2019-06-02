59c3fd7f877ce36b5b5ba3cacc12854b3d252da8
Jeremy Greer <jex.grizzle@gmail.com>
2019-06-01 21:33:24 -0700

    Use disjoint? to check overlapping ranges

--- lib/intersections.ex
+++ lib/intersections.ex
@@ -1,6 +1,8 @@
 defmodule ElementsWeb.Intersections do
   def ranges_intersect({left, right}, {min, max}) do
-    not (right < min or left > max)
+    a = Range.new(left, right)
+    b = Range.new(min, max)
+    not Range.disjoint?(a, b)
   end
 
   def boxes_intersect(a, b) do
--- test/elements_web/intersections_test.exs
+++ test/elements_web/intersections_test.exs
@@ -2,19 +2,6 @@ defmodule ElementsWeb.IntersectionsTest do
   use ExUnit.Case
   import ElementsWeb.Intersections
 
-  test "ranges_intersect" do
-    assert ranges_intersect({0, 3}, {1, 4}) == true
-    assert ranges_intersect({0, 3}, {4, 5}) == false
-    assert ranges_intersect(
-      { 0, 9 },
-      { 3, 8 }
-    ) == true
-    assert ranges_intersect(
-      { 3, 8 },
-      { 0, 9 }
-    ) == true
-  end
-
   test "boxes_intersect" do
     assert boxes_intersect(
       %{ top: 0, bottom: 15, left: 0, right: 9 },
